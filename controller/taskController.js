d1501ba8cdc9ffadf73df19c35559b0337351151
Santosh Kumar <santosh.283143@gmail.com>
2022-10-17 16:29:58 +0530

    added update and delete task

--- controller/taskController.js
+++ controller/taskController.js
@@ -10,7 +10,12 @@ const taskController = {
         res.render('create')
     },
     edit: async (req, res) => {
-        res.render('update')
+        const id = req.params.id; //to read ref id from router
+
+        const data = await Task.findById({ _id: id })
+        // console.log(`single data = `, data)
+
+        res.render('update', { task: data })
     },
     createTask: async (req, res) => {
         // console.log(`data = `, req.body);
@@ -20,6 +25,19 @@ const taskController = {
         // console.log(`data =`, newTask)
         newTask.save()
         console.log(`task created successfully`)
+        res.redirect('/')
+    },
+    updateTask: async (req, res) => {
+        const data = req.body
+        // console.log(`updated data = `, data)
+
+        await Task.findByIdAndUpdate({ _id: req.params.id }, data)
+        res.redirect('/')
+    },
+    deleteTask: async (req, res) => {
+        const id = req.params.id
+        await Task.findByIdAndDelete({ _id: id })
+        console.log(`deleted successfully`);
         res.redirect('/')
     }
 }
